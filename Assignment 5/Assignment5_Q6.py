5a7a8d9e30e48c9780e3bda99566bdb930d57285
Dhanu1290 <dhanu1290@gmail.com>
2026-06-22 23:48:04 +0530

    files add
    new

---
+++ Assignment 5/Assignment5_Q6.py
@@ -0,0 +1,6 @@
+x = None
+print(type(x))         #noneType
+print(x == False)      #Boolean ---comparison with None returns False
+
+# output : <class 'NoneType'>
+#False
---
+++ Assignment 5/Assignment5_Q9.py
@@ -0,0 +1,3 @@
+s = {10, 20, 30, 10, 20}  #set Stores unique elements only,duplicate elements are removed
+
+# output : {10, 20, 30}
