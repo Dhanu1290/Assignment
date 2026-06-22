5bff922cd08c511bda1b48ad1c790984c3df5cd2
Dhanu1290 <dhanu1290@gmail.com>
2026-06-22 22:52:25 +0530

    folder
    file update

---
+++ Assignment 4/Assignment4_Q1.py
@@ -0,0 +1,12 @@
+# Accept : Multiple parameters
+# Return : Multiplication of two numbers
+
+def Multiplication(No1,No2):
+    return No1*No2
+
+def main():
+    Ret1= Multiplication(10,5)
+    print("Multiplication is :",Ret1)
+   
+if __name__=="__main__":                                #1
+    main()
