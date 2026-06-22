e7b27856ccdaa378487333736131f83f254de1a9
Dhanu1290 <dhanu1290@gmail.com>
2026-06-22 15:51:25 +0530

    Folder
    Created folder for the structure

--- Q1.py
+++
@@ -1,7 +0,0 @@
-x = 10
-
-print("Value:", x)
-
-print("Type:", type(x))
-
-print("Memory Address:", id(x))
--- Q2.py
+++
@@ -1,9 +0,0 @@
-a = 10
-b = 10
-c = [10]
-d = [10]
-
-print(id(a))  # same memory
-print(id(b))  # same memory
-print(id(c))  # separate memory
-print(id(d))  # separate memory
